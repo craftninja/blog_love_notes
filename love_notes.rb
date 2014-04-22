ca89bb9d32543b7fb4443e9aeb047b4a64ff88bc
EmilyPlatzer <emilyplatzer@gmail.com>
2014-04-21 22:22:14 -0600

    User can add love notes and see them all listed on homepage.

---
+++ love_notes.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+
+class LoveNotes < Sinatra::Application
+  LoveNoteLibrary = Array.new
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    LoveNoteLibrary << params[:love_note]
+    redirect '/'
+  end
+
+end
